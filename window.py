4c08e43bfed854abed7103be0dcd0acef53b5caf
etylu <etlu03@gmail.com>
2022-12-29 19:54:19 -0800

    Added more to shell visuals

--- window.py
+++ window.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from datetime import date, datetime
 
-def retrieve_cached():
+def cache_login():
   weekdays = {0: 'Mon',
               1: 'Tue',
               2: 'Wed',
@@ -23,20 +23,30 @@ def retrieve_cached():
     f.write(login)
 
 if __name__ == '__main__':
-  retrieve_cached()
+  with open("_cache/last_login", "r", encoding="utf-8") as f:
+    last_login = f.read()
+  cache_login()
 
   root = tk.Tk()
   root.geometry("569x343")
   root.title("User — client@users — ~ — -psh — 80x24")
   root.resizable(False, False)
 
-  with open("_cache/last_login", "r", encoding="utf-8") as f:
-    last_login = f.read()
+  entry = tk.Entry(root,
+                   width=70,
+                   font=("Courier", 14),
+                   highlightthickness=0,
+                   borderwidth=0)
+  entry.place(anchor=tk.NW, x=20, y=22)
 
-  print(last_login)
   label = tk.Label(root, 
                    text=f"Last login: {last_login} on ttys",
                    font=("Courier", 14))
-  label.place(anchor = tk.NW, x = 0, y = 0)
+  label.place(anchor=tk.NW, x=0, y=0)
+
+  prompt = tk.Label(root,
+                    text=">",
+                    font=("Courier", 14))
+  prompt.place(anchor=tk.NW, x=0, y=22)
 
   root.mainloop()
